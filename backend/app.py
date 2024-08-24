a1371c8090bced989a2e3c87bc789fd5d8542cd9
philip-how <howardphilip7@gmail.com>
2024-08-24 21:50:32 +1000

    Fixing filenames for HTML docs

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,8 @@ def login():
         if backend.login_checker(attempt):
             session['username'] = attempt
             return redirect(url_for('friend_list'))
-        return render_template("HTML/login.html")
-    return render_template("HTML/login.html")
+        return render_template("../HTML/login.html")
+    return render_template("../HTML/login.html")
 
 @app.route("/sign_up", methods=["GET","POST"])
 def sign_up():
@@ -24,19 +24,19 @@ def sign_up():
         if not backend.add_user(name,username,password):
             session['username'] = username
             return redirect(url_for('home_page'))
-        return render_template("HTML/sign_up.html")
-    return render_template("HTML/sign_up.html")
+        return render_template("../HTML/sign_up.html")
+    return render_template("../HTML/sign_up.html")
 
 @app.route("/friend_list")
 def friend_list():
     username = session['username']
     me = backend.find_username(username)
     friend = me.online_friends()
-    return render_template("HTML/friend_list.html", friend_list=friend)
+    return render_template("../HTML/friend_list.html", friend_list=friend)
     
 @app.route("/home_page")
 def home_page():
-    return render_template("HTML/home_page.html")
+    return render_template("../HTML/home_page.html")
 
 @app.route("/profile_page", methods=["GET", "POST"])
 def profile_page():
@@ -45,7 +45,7 @@ def profile_page():
         me = backend.find_username(username)
         username = me.change_ics(ics_link)
     username = session['username']
-    return render_template("HTML/profile_page.html", username=username)
+    return render_template("../HTML/profile_page.html", username=username)
 
 @app.route("/sign_out")
 def sign_out():
