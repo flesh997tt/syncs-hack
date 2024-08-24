4b94809c2fe9a2626ba766ae89f05424981b9a87
philip-how <howardphilip7@gmail.com>
2024-08-24 15:52:22 +1000

    Added helper functions for future API calls

--- backend/backend.py
+++ backend/backend.py
@@ -4,10 +4,33 @@ class backend:
     def __init__(self, filename: str):
         self.filename = filename
     
-    def parsecalendar(self):
+    def parse_file(self):
         fobj = open(self.filename)
         rows = fobj.readlines()
         self.people = []
         for row in rows:
             self.people.append(Person(row))
-    
+        for person in self.people:
+            person.fix_people(self.people)
+        fobj.close()
+
+    def save_file(self):
+        fobj = open(self.filename, "w")
+        for person in self.people:
+            print(person.format(),file=fobj)
+        fobj.close()
+
+    def add_user(self, name, username, password):
+        for person in self.people:
+            if username == person.get_username():
+                return None
+        self.people.append(Person([name, username, password]))
+
+    def login_checker(self, username, password):
+        for person in self.people:
+            if username == person.get_username():
+                if person.check_password(password):
+                    return True
+                else:
+                    return False
+        return False
